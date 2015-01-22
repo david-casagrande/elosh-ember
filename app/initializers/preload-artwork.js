94c9f3d5ef0a92f2e31e3a3f11348d2652c675bd
David Casagrande <dncasagrande@gmail.com>
2015-01-21 21:07:52 -0500

    comment out initializer preload for now

--- app/initializers/preload-artwork.js
+++ app/initializers/preload-artwork.js
@@ -2,8 +2,8 @@ export function initialize(container, application) {
   //console.log(window.ELOSH_PRELOADED_ARTWORK)
   //console.log(container.lookup('store:main'), application);
 
-  var store = container.lookup('store:main');
-  store.pushPayload(window.ELOSH_PRELOADED_ARTWORK);
+  //var store = container.lookup('store:main');
+  //store.pushPayload(window.ELOSH_PRELOADED_ARTWORK);
   // application.inject('route', 'foo', 'service:foo');
 }
 
